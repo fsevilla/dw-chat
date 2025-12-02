e834b377a2296a83f550728593e78804857a5816
Francisco Sevilla <fsevilla@gmail.com>
2025-12-02 17:37:04 -0600

    Adding reactive forms to the contact component

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,25 +1,45 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { 
+  FormsModule, 
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 
 @Component({
   selector: 'dw-contact',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
-  name = 'John Doe'
-  email = 'john@doe.com'
-  phone = ''
-  subject = ''
-  message = ''
+
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]], // required, minlength
+      email: ['', [Validators.required, Validators.email]],  // required, email
+      phone: '1234', // numeric
+      subject: 'suggestion', // required
+      message: 'lorem ipsum' // required
+    })
+  }
 
   onSubmit() {
-    console.log('You will submit the data')
+    if (this.form.valid) {
+      const data = this.form.getRawValue();
+      console.log('You will submit the data?', data, this.form);
+    } else {
+      console.log('You are missing data...', this.form)
+    }
   }
 
-  onKeyup(e: KeyboardEvent) {
-    console.log('The name is: ', (e.target as HTMLInputElement).value)
+  onCancel(e: MouseEvent) {
+    e.preventDefault();
   }
+
+
 }
